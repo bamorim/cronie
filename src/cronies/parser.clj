2fb69615cd9278bd23c225dd32a9e89a9f70aef6
Antonis Pishias <espen.folke@gmail.com>
2020-01-13 23:30:52 +0000

    Parsing for hours and minutes

--- src/cronies/parser.clj
+++ src/cronies/parser.clj
@@ -2,16 +2,14 @@
   (:require [clojure.string :as s]))
 
 (defn parse-term [term]
-  (let [[word start end step]
-        (re-find #"(\d+)\-(\d+)\/(\d+)" term)
-        {:start start :end end :step step})))
+  (condp re-find term
+    #"(\d{1,2})\-(\d{1,2})\/(\d{1,2})" :>> #(hash-map :start (nth % 1) :end (nth % 2) :step (nth % 3))
+    #"(\d{1,2})\-(\d{1,2})" :>> #(hash-map :start (nth % 1) :end (nth % 2) :step nil)
+    #"(\d{1,2}|\*)\/(\d{1,2})" :>> #(hash-map :start (nth % 1) :end nil :step (nth % 2))
+    #"(\d{1,2}|\*)" :>> #(hash-map :start (nth % 1) :end nil :step nil)))
 
 
 (defn parse-cron [cron]
   (as-> cron x
     (s/split x #" ")
     (map parse-term x)))
-
-#check condp for different regex cases
-
-(parse-cron "* * * * * ")
--- test/cronies/parser_test.clj
+++ test/cronies/parser_test.clj
@@ -5,4 +5,19 @@
 (deftest a-test
   (testing "Extract info."
     (is (= {:start "*" :end nil :step nil}
-           (first (parse "* * * * *"))))))
+           (first (parse-cron "* * * * *"))))))
+
+(deftest condp-test
+  (testing "conditional regex function"
+    (is (= {:start "2" :end "39" :step nil}
+           (parse-term "2-39")))
+    (is (= {:start "10" :end nil :step nil}
+           (parse-term "10")))
+    (is (= {:start "12" :end "59" :step "2"}
+           (parse-term "12-59/2")))
+    (is (= {:start "12" :end nil :step "2"}
+           (parse-term "12/2")))
+    (is (= {:start "*" :end nil :step nil }
+           (parse-term "*")))
+    (is (= {:start "*" :end nil :step "4"}
+           (parse-term "*/4")))))
