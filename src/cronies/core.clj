6e7915609f58b3f7e5407e085a87f5c72bfc195a
Bernardo Amorim <contato@bamorim.com>
2020-01-13 20:05:42 +0000

    defining functions

--- src/cronies/core.clj
+++ src/cronies/core.clj
@@ -1,6 +1,19 @@
 (ns cronies.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; node: (start end step)
+; cron: (minutes hour day_of_month month day_of_week)
+
+(defn parse
+  "Parses a cron string into a map of maps"
+  [_string]
+  {})
+
+(defn step
+  "Returns the next plausible date time to be tested given a datetime and a cron"
+  [dt _cron]
+  [dt])
+
+(defn test_cron
+  "Checks if a datetime is valid given a cron"
+  [_dt _cron]
+  false)
